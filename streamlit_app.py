6a327205491d699ea2a8fc09da4ab7b5a67c906d
min66633 <minuk3614@gmail.com>
2026-02-08 03:20:18 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,6 @@ from datetime import date, timedelta
 # ===============================
 st.set_page_config(layout="wide")
 st.title("📊 Options Greeks & Gamma Exposure Dashboard")
-st.write("Last update:", pd.Timestamp.now())
-
 
 POLYGON_API_KEY = st.secrets["mD0LX0bzkc3sIUH3Hs0lwNucRo90HtML"]
 
